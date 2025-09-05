1ca0db32081acd40afab54c70705134f60152014
unishAryal <unish@wylight.technology>
2025-09-05 15:57:53 -0400

    chore: added signedOut

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,8 +1,9 @@
-import { signUp } from "../controller/authController.js";
+import { signUp, signIn } from "../controller/authController.js";
 import express from 'express';
 
 const authRouter = express.Router();
 
 authRouter.post('/signUp', signUp);
+authRouter.post('/signIn',signIn )
 
 export default authRouter;
