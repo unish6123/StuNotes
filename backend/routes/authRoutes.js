6de5c0340fbc71fc7221f955fe5354d2eff85790
unishAryal <unish@wylight.technology>
2025-09-07 10:45:07 -0400

    feat/forgotPassword

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,9 +1,11 @@
-import { signUp, signIn } from "../controller/authController.js";
+import { signUp, signIn, sendForgotPasswordOtp, signOut } from "../controller/authController.js";
 import express from 'express';
 
 const authRouter = express.Router();
 
 authRouter.post('/signUp', signUp);
-authRouter.post('/signIn',signIn )
+authRouter.post('/signIn',signIn );
+authRouter.post('/forgotPassword', sendForgotPasswordOtp);
+authRouter.get('/signOut', signOut)
 
 export default authRouter;
