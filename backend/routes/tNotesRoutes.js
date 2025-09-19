008ca365106375b9bbaf94f49d296355ea5a480a
unishAryal <unish@wylight.technology>
2025-09-19 15:54:16 -0400

    implemented quiz score functionality

--- backend/routes/tNotesRoutes.js
+++ backend/routes/tNotesRoutes.js
@@ -1,12 +1,13 @@
 import express from 'express';
-import  { saveTNotes, getSavedTNotes, getQuiz } from '../controller/transcribeNoteController.js'
+import  { saveTNotes, getSavedTNotes, getQuiz, saveQuizScore } from '../controller/transcribeNoteController.js'
 import userAuth from '../middleware/authMiddlewear.js';
 
 const tNotesRouter = express.Router();
 
 
-tNotesRouter.post('/saveTranscribe',userAuth, saveTNotes);
+tNotesRouter.post('/saveNotes',userAuth, saveTNotes);
 tNotesRouter.get('/getNotes',userAuth, getSavedTNotes);
 tNotesRouter.post('/getQuiz',userAuth, getQuiz);
+tNotesRouter.post('/score', userAuth, saveQuizScore )
 
 export default tNotesRouter;
